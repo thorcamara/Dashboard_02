c337bf1533084c6e8d7efca1720fbc3983a3279d
thorcamara <thorcamaraalthoff@gmail.com>
2023-10-05 22:11:24 -0300

    Added more EventListeners and const

--- script.js
+++ script.js
@@ -32,3 +32,25 @@ searchBtn.addEventListener('click', function (e) {
     }
   }
 });
+
+window.addEventListener('resize', () => {
+  if (window.innerWidth < 768) {
+    sideBar.classList.add('close');
+  } else {
+    sideBar.classList.remove('close');
+  }
+  if (window.innerWidth > 576) {
+    searchBtnIcon.classList.replace('bx-x', 'bx-search');
+    searchForm.classList.remove('show');
+  }
+});
+
+const toggler = document.getElementById('theme-toggle');
+
+toggler.addEventListener('change', function () {
+  if (this.checked) {
+    document.body.classList.add('dark');
+  } else {
+    document.body.classList.remove('dark');
+  }
+});
