c524df3fe9ce1a81e52b8e169b91f2507d5bb4c6
thorcamara <thorcamaraalthoff@gmail.com>
2023-10-05 22:10:50 -0300

    Added EventListeners and constants

--- script.js
+++ script.js
@@ -12,3 +12,23 @@ sideLinks.forEach(item => {
 
 const menuBar = document.querySelector('.content nav .bx.bx-menu');
 const sideBar = document.querySelector('.sidebar');
+
+menuBar.addEventListener('click', () => {
+  sideBar.classList.toggle('close');
+});
+
+const searchBtn = document.querySelector('.content nav form .form-input button');
+const searchBtnIcon = document.querySelector('.content nav form .form-input button .bx');
+const searchForm = document.querySelector('.content nav form');
+
+searchBtn.addEventListener('click', function (e) {
+  if (window.innerWidth < 576) {
+    e.preventDefault;
+    searchForm.classList.toggle('show');
+    if (searchForm.classList.contains('show')) {
+      searchBtnIcon.classList.replace('bx-search', 'bx-x');
+    } else {
+      searchBtnIcon.classList.replace('bx-x', 'bx-search');
+    }
+  }
+});
